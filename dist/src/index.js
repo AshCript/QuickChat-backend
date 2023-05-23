2f66085028e0733c03479a6b5cc49097b0e96256
AshCript <asjosvah@gmail.com>
2023-05-23 17:54:30 +0300

    Rollback to the last project safe code

--- dist/src/index.js
+++ dist/src/index.js
@@ -1,13 +1,15 @@
 "use strict";
-var __importDefault = (this && this.__importDefault) || function (mod) {
-    return (mod && mod.__esModule) ? mod : { "default": mod };
-};
 Object.defineProperty(exports, "__esModule", { value: true });
-const express_1 = __importDefault(require("express"));
+const App_1 = require("./App");
+const ContactRoute_1 = require("./routes/ContactRoute");
+const MessageRoute_1 = require("./routes/MessageRoute");
+const UserRoute_1 = require("./routes/UserRoute");
 const port = Number(process.env.PORT) || 3005;
-const app = (0, express_1.default)();
-app.get('/', (_req, res) => {
-    return res.send('Hello World');
-});
-app.listen(port, () => { console.log("Server started"); });
+const routes = [
+    new UserRoute_1.UserRoute(),
+    new ContactRoute_1.ContactRoute(),
+    new MessageRoute_1.MessageRoute(),
+];
+const app = new App_1.App(port, routes);
+app.listen();
 //# sourceMappingURL=index.js.map
